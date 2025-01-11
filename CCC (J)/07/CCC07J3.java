4afd548aceb90ea78bfb7d165141cf3909af8203
cw0527 <107016023+cw0527@users.noreply.github.com>
2025-01-11 23:49:30 +0000

    Edited formatting

--- CCC (J)/07/CCC07J3.java
+++ CCC (J)/07/CCC07J3.java
@@ -1,30 +1,19 @@
 import java.util.Scanner;
-public class CCC07J3
-{
-    public static void main(String[] args)
-    {
+public class CCC07J3 {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int[] array = {100, 500, 1000, 5000, 10000, 25000, 50000, 100000, 500000, 1000000};
         int cases = input.nextInt();
-        for (int i = 0; i < cases; i++)
-        {
+
+        for (int i = 0; i < cases; i++) {
             int n = input.nextInt();
             array[n-1] = 0;
         }
+
         int sum = 0;
-        for (int i = 0; i < 10; i++)
-        {
-            sum = sum + array[i];
-        }
+        for(int i = 0; i < 10; i++) sum +=array[i];
         int avg = sum/(10-cases);
         int offer = input.nextInt();
-        if (offer>avg)
-        {
-            System.out.println("deal");
-        }
-        else
-        {
-            System.out.println("no deal");
-        }
+        System.out.println(offer > avg ? "deal" : "no deal");
     }
 }
