d02f5efdc4c96bd96bd17f6417bed8ddd7189973
whutrx <curtisuwu@gmail.com>
2025-01-13 07:00:23 +0000

    idk bro i need to do coding hw and i broke my intellj just ignore this

---
+++ CCC (S)/04/CCC04S3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class CCC04S3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+
+        input.close();
+    
+}
+
+}
