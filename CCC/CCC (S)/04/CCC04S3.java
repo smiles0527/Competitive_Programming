8a524c58c09b4f3f67eb8bd51256a048645cf148
cw0527 <iscurt.w@gmail.com>
2025-01-14 12:39:44 -0800

    improved 04s3.java

--- CCC/CCC (S)/04/CCC04S3.java
+++ CCC/CCC (S)/04/CCC04S3.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.*;
 
 public class CCC04S3 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -7,40 +6,70 @@ public class CCC04S3 {
     static boolean[][] vis = new boolean[10][10];
 
     public static void main(String[] args) throws IOException {
+        // Read the grid input
         for (int i = 0; i < 10; i++) {
             String[] cols = br.readLine().split(" ");
-            System.arraycopy(cols, 0, grid[i], 1, 9);
+            for (int j = 0; j < 9; j++) {
+                grid[i][j + 1] = cols[j]; 
+            }
         }
+
+        // Process each cell in the grid
         for (int i = 0; i < 10; i++) {
             for (int j = 1; j <= 9; j++) {
-                vis = new boolean[10][10];
+                // Reset the vis array for each new cell
+                for (int k = 0; k < 10; k++) {
+                    for (int l = 0; l < 10; l++) {
+                        vis[k][l] = false;
+                    }
+                }
                 dfs(i, j);
             }
         }
+
+        // Print the resulting grid
         for (int i = 0; i < 10; i++) {
-            System.out.println(String.join(" ", Arrays.copyOfRange(grid[i], 1, 10)));
+            for (int j = 1; j <= 9; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 
     public static int dfs(int r, int c) {
-        if (isNumeric(grid[r][c])) return Integer.parseInt(grid[r][c]);
-        if (vis[r][c] || grid[r][c].equals("*")) return -1;
-        vis[r][c] = true;
+        if (isNumeric(grid[r][c])) {
+            return Integer.parseInt(grid[r][c]);
+        }
+        if (vis[r][c] || grid[r][c].equals("*")) {
+            return -1;
+        }
+
+        vis[r][c] = true; 
         String[] depend = grid[r][c].split("\\+");
         int sum = 0;
-        for (String cell : depend) {
-            int ret = dfs(cell.charAt(0) - 'A', cell.charAt(1) - '0');
+
+        for (int i = 0; i < depend.length; i++) {
+            int ret = dfs(depend[i].charAt(0) - 'A', depend[i].charAt(1) - '0');
             if (ret == -1) {
                 grid[r][c] = "*";
                 return -1;
             }
             sum += ret;
         }
-        grid[r][c] = String.valueOf(sum);
+
+        grid[r][c] = String.valueOf(sum); 
         return sum;
     }
 
     public static boolean isNumeric(String strNum) {
-        return strNum != null && strNum.matches("\\d+");
+        if (strNum == null) {
+            return false;
+        }
+        for (int i = 0; i < strNum.length(); i++) {
+            if (!Character.isDigit(strNum.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
