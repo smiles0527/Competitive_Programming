2398a6e180a63c0a016a7c3356dd74d591582867
cw0527 <iscurt.w@gmail.com>
2025-02-19 10:33:03 -0800

    updated cs5

--- official(c)/cs5.cpp
+++ official(c)/cs5.cpp
@@ -1,113 +1,101 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
 using ll = long long;
  
-const int MODVAL = 1000000+3;
-const ll NEG_INF = -1e18;
+const int RMAX = 1000003;
+const ll NEG = -1e18;
+ 
+struct Node {
+    ll k, l;
+};
+ 
+inline Node combine(const Node &L, const Node &R) {
+    return {L.k + R.k, max(L.l + R.k, R.l)};
+}
  
 struct SegTree {
-    int n;
-    vector<ll> seg, lazy;
-    SegTree(int n): n(n) {
-        seg.assign(4*n, NEG_INF);
-        lazy.assign(4*n, 0);
+    int size;
+    vector<Node> tree;
+    SegTree(int n) {
+        size = 1;
+        while(size < n) size *= 2;
+        tree.assign(2*size, {0, NEG});
     }
-    void pushDown(int idx, int l, int r) {
-        if(lazy[idx]!=0) {
-            int mid=(l+r)/2;
-            seg[idx*2] += lazy[idx];
-            seg[idx*2+1] += lazy[idx];
-            lazy[idx*2] += lazy[idx];
-            lazy[idx*2+1] += lazy[idx];
-            lazy[idx] = 0;
+    void build(vector<Node>& arr) {
+        for (int i = 0; i < (int)arr.size(); i++) {
+            tree[size + i] = arr[i];
+        }
+        for (int i = arr.size(); i < size; i++) {
+            tree[size + i] = {0, NEG};
+        }
+        for (int i = size - 1; i > 0; i--) {
+            tree[i] = combine(tree[2*i], tree[2*i+1]);
         }
     }
-    void updateRange(int idx, int l, int r, int ql, int qr, ll val) {
-        if(ql>r || qr<l)return;
-        if(ql<=l && r<=qr) {
-            seg[idx] += val;
-            lazy[idx] += val;
-            return;
+    void update(int pos, Node val) {
+        pos += size - 1;
+        tree[pos] = val;
+        for(pos /= 2; pos; pos /= 2)
+            tree[pos] = combine(tree[2*pos], tree[2*pos+1]);
+    }
+    Node query(int l, int r) {
+        Node L = {0, NEG}, Rn = {0, NEG};
+        l += size - 1; r += size - 1;
+        while(l <= r) {
+            if(l % 2 == 1) { L = combine(L, tree[l]); l++; }
+            if(r % 2 == 0) { Rn = combine(tree[r], Rn); r--; }
+            l /= 2; r /= 2;
         }
-        pushDown(idx,l,r);
-        int mid=(l+r)/2;
-        updateRange(idx*2,l,mid,ql,qr,val);
-        updateRange(idx*2+1,mid+1,r,ql,qr,val);
-        seg[idx]=max(seg[idx*2], seg[idx*2+1]);
+        return combine(L, Rn);
     }
-    void updatePoint(int idx, int l, int r, int pos, ll val) {
-        if(l==r) {
-            seg[idx] = val;
-            return;
-        }
-        pushDown(idx,l,r);
-        int mid=(l+r)/2;
-        if(pos<=mid) updatePoint(idx*2,l,mid,pos,val);
-        else updatePoint(idx*2+1,mid+1,r,pos,val);
-        seg[idx]=max(seg[idx*2], seg[idx*2+1]);
-    }
-    ll query(int idx, int l, int r, int ql, int qr) {
-        if(ql>r || qr<l)return NEG_INF;
-        if(ql<=l && r<=qr)return seg[idx];
-        pushDown(idx,l,r);
-        int mid=(l+r)/2;
-        return max(query(idx*2,l,mid,ql,qr), query(idx*2+1,mid+1,r,ql,qr));
-    }
-    void updRange(int l, int r, ll val) { updateRange(1,0,n-1,l,r,val); }
-    void updPoint(int pos, ll val) { updatePoint(1,0,n-1,pos,val); }
-    ll qry(int l, int r) { return query(1,0,n-1,l,r); }
 };
  
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
- 
     int Q; cin >> Q;
-    SegTree seg(MODVAL);
-    vector<ll> base(MODVAL);
-    for (int i = 0; i < MODVAL; i++){
-        base[i] = i - 1;
-        seg.updPoint(i, NEG_INF);
+    vector<pair<int,int>> tasks; tasks.reserve(Q+1);
+    tasks.push_back({0,0});
+    vector<ll> A(RMAX+1,0);
+    vector<int> cnt(RMAX+1,0);
+    vector<Node> base(RMAX);
+    for (int i = 1; i <= RMAX; i++){
+        base[i-1] = {0, (ll)i};
     }
-    vector<pair<int,int>> tasks;
-    tasks.reserve(Q);
-    vector<ll> Tval(MODVAL, 0);
-    ll totalTime = 0, ans = 0;
+    SegTree seg(RMAX);
+    seg.build(base);
+    set<int> active;
+    ll ans = 0;
+    int addCount = 0;
     for (int qi = 0; qi < Q; qi++){
-        char typ; cin >> typ;
-        if(typ=='A'){
-            ll s_, t_;
-            cin >> s_ >> t_;
-            int s = (int)((s_ + ans) % MODVAL);
-            int t = (int)((t_ + ans) % MODVAL);
-            if(s < 1) s = 1;
-            if(t < 1) t = 1;
+        char op; cin >> op;
+        if(op=='A'){
+            ll sp, tp; cin >> sp >> tp;
+            int s = (int)((sp + ans) % RMAX);
+            int t = (int)((tp + ans) % RMAX);
+            if(s < 1) s = 1; if(t < 1) t = 1;
+            addCount++;
             tasks.push_back({s,t});
-            totalTime += t;
-            if(Tval[s] == 0){
-                seg.updPoint(s, seg.qry(s,s)==NEG_INF ? (s - 1) : seg.qry(s,s));
-                seg.updPoint(s, s - 1);
-            }
-            Tval[s] += t;
-            if(s+1 < MODVAL) seg.updRange(s+1, MODVAL-1, -t);
+            cnt[s]++;
+            A[s] += t;
+            seg.update(s, {A[s], (ll)s + A[s]});
+            active.insert(s);
         } else {
-            ll i_; cin >> i_;
-            int i = (int)((i_ + ans) % MODVAL);
-            if(i < 1) i = 1;
-            int idx = i - 1;
-            int s = tasks[idx].first, t = tasks[idx].second;
+            ll ip; cin >> ip;
+            int idx = (int)((ip + ans) % RMAX);
+            if(idx < 1) idx = 1;
+            auto pr = tasks[idx];
+            int s = pr.first, t = pr.second;
             tasks[idx] = {0,0};
-            totalTime -= t;
-            Tval[s] -= t;
-            if(Tval[s] == 0){
-                seg.updPoint(s, NEG_INF);
-            }
-            if(s+1 < MODVAL) seg.updRange(s+1, MODVAL-1, t);
+            cnt[s]--;
+            A[s] -= t;
+            seg.update(s, {A[s], (ll)s + A[s]});
+            if(cnt[s]==0) active.erase(s);
         }
-        ll bestCandidate = seg.qry(0, MODVAL-1);
-        if(bestCandidate < 0) bestCandidate = 0;
-        ans = totalTime + bestCandidate;
+        int mx = active.empty() ? 1 : *active.rbegin();
+        Node res = seg.query(1, mx);
+        ll T = max(1LL + res.k, res.l);
+        ans = T - 1;
         cout << ans << "\n";
     }
     return 0;
