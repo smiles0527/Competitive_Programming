aaa785fb2b99da007cc24415a022a385e45618f7
whutrx <curtisuwu@gmail.com>
2025-02-18 23:36:36 -0800

    added my files for tmrw sob!

--- CCC/CCC (J)/23/CCC23J5.java
+++ CCC/CCC (J)/23/CCC23J5.java
@@ -7,48 +7,132 @@ public class CCC23J5 {
     static String word;
     static int count;
 
-    public static void backtrack(int r, int c, int index, int dr, int dc) {
-        if (index == word.length()) {
+    public static void backtrack(int r, int c, int index, int dr, int dc, boolean turn) {
+        if (r < 0 || r >= row || c < 0 || c >= col || index == word.length() || (map[r][c] != word.charAt(index))) {
+            return;
+        }
+        if (index == word.length()-1)
+        {
             count++;
             return;
         }
-        if (r < 0 || r >= row || c < 0 || c >= col || map[r][c] != word.charAt(index)) {
-            return;
+        if (turn)
+        {
+            backtrack(r + dr, c + dc, index + 1, dr, dc, true);
         }
-        backtrack(r + dr, c + dc, index + 1, dr, dc);
+        else
+        {
+            backtrack(r + dr, c + dc, index + 1, dr, dc, false);
+            if (index>0)
+            {
+                if (dr == 1 && dc == 0) //down
+                {
+                    int ndr = 0;
+                    int ndc = -1;
+                    backtrack(r + ndr, c + ndc, index + 1, ndr, ndc, true);
+                    ndr = 0;
+                    ndc = 1;
+                    backtrack(r + ndr, c + ndc, index + 1, ndr, ndc, true);
+                }
+                if (dr == -1 && dc == 0) //up
+                {
+                    int ndr = 0;
+                    int ndc = -1;
+                    backtrack(r + ndr, c + ndc, index + 1, ndr, ndc, true);
+                    ndr = 0;
+                    ndc = 1;
+                    backtrack(r + ndr, c + ndc, index + 1, ndr, ndc, true);
+                }
+                if (dr == 0 && dc == 1) //left right
+                {
+                    int ndr = -1;
+                    int ndc = 0;
+                    backtrack(r + ndr, c + ndc, index + 1, ndr, ndc, true);
+                    ndr = 1;
+                    ndc = 0;
+                    backtrack(r + ndr, c + ndc, index + 1, ndr, ndc, true);
+                }
+                if (dr == 0 && dc == -1) //right left
+                {
+                    int ndr = -1;
+                    int ndc = 0;
+                    backtrack(r + ndr, c + ndc, index + 1, ndr, ndc, true);
+                    ndr = 1;
+                    ndc = 0;
+                    backtrack(r + ndr, c + ndc, index + 1, ndr, ndc, true);
+                }
+                if (dr == -1 && dc == 1) //bottom left top right
+                {
+                    int ndr = -1;
+                    int ndc = -1;
+                    backtrack(r + ndr, c + ndc, index + 1, ndr, ndc, true);
+                    ndr = 1;
+                    ndc = 1;
+                    backtrack(r + ndr, c + ndc, index + 1, ndr, ndc, true);
+                }
+                if (dr == 1 && dc == 1) //bottom right top left
+                {
+                    int ndr = 1;
+                    int ndc = -1;
+                    backtrack(r + ndr, c + ndc, index + 1, ndr, ndc, true);
+                    ndr = -1;
+                    ndc = 1;
+                    backtrack(r + ndr, c + ndc, index + 1, ndr, ndc, true);
+                }
+                if (dr == 1 && dc == -1) //top right bottom left
+                {
+                    int ndr = -1;
+                    int ndc = -1;
+                    backtrack(r + ndr, c + ndc, index + 1, ndr, ndc, true);
+                    ndr = 1;
+                    ndc = 1;
+                    backtrack(r + ndr, c + ndc, index + 1, ndr, ndc, true);
+                }
+                if (dr == -1 && dc == -1) //top left bottom right
+                {
+                    int ndr = 1;
+                    int ndc = -1;
+                    backtrack(r + ndr, c + ndc, index + 1, ndr, ndc, true);
+                    ndr = -1;
+                    ndc = 1;
+                    backtrack(r + ndr, c + ndc, index + 1, ndr, ndc, true);
+                }
+            }
+        }
+
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        word = scanner.next();
-        row = scanner.nextInt();
-        col = scanner.nextInt();
+        word = input.next();
+        row = input.nextInt();
+        col = input.nextInt();
 
         map = new char[row][col];
         for (int r = 0; r < row; r++) {
-            String words = scanner.next();
-            for (int c = 0; c < col-c; c++) {
-                map[r][c] = words.charAt(c);
+            for (int c = 0; c < col; c++) {
+                String words = input.next();
+                map[r][c] = words.charAt(0);
             }
         }
 
         count = 0;
         for (int r = 0; r < row; r++) {
             for (int c = 0; c < col; c++) {
-                backtrack(r, c, 0, 1, 0);   //horizontal (left to right)
-                backtrack(r, c, 0, -1, 0);  // horizontal (right to left)
-                backtrack(r, c, 0, 0, 1);   // vertical (top to bottom)
-                backtrack(r, c, 0, 0, -1);  // vertical (bottom to top)
-                backtrack(r, c, 0, 1, 1);   // diagonal (top-left to bottom-right)
-                backtrack(r, c, 0, -1, -1); // diagonal (bottom-right to top-left)
-                backtrack(r, c, 0, 1, -1);  // diagonal (bottom-left to top-right)
-                backtrack(r, c, 0, -1, 1);  // diagonal (top-right to bottom-left)
+                backtrack(r, c, 0, 1, 0, false);   //h lr
+                backtrack(r, c, 0, -1, 0, false);  // h rl
+                backtrack(r, c, 0, 0, 1, false);   // v tb
+                backtrack(r, c, 0, 0, -1, false);  // v bt
+                backtrack(r, c, 0, 1, 1, false);   // d tl br
+                backtrack(r, c, 0, -1, -1, false); // d br tl
+                backtrack(r, c, 0, 1, -1, false);  // d bl tr
+                backtrack(r, c, 0, -1, 1, false);  // d tr bl
             }
         }
 
         System.out.println(count);
-        scanner.close();
+        input.close();
     }
 }
 
