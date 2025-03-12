7c312f18e28f9bb26aec8d3fa9e320aeab9dddfb
whutrx <curtisuwu@gmail.com>
2025-03-12 10:34:05 -0700

    Update readme.cpp

---
+++ readme.cpp
@@ -0,0 +1,6 @@
+wait so im being crahsed on?
+  wtf is happening bro
+what do i do
+  dyk what she said abt me
+cuz like ik i ahd fault but the only thing i did was tell somebody i trust about a truth wchih last time i checked isnt really something wrong
+bro im so scared
