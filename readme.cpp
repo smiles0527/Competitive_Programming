360a350c4001929050fa339b06a6806d8d043c7b
cw0527 <iscurt.w@gmail.com>
2025-03-12 01:46:37 -0700

    readme.cpp

---
+++ readme.cpp
@@ -0,0 +1,15 @@
+ok so im out of the thing as according to iris mom she knows me well (she does) and she just thinks i can grow up a bit and 
+then like they were just talkin together about how to educate peple (yes funny hwo they just turned sides)
+
+and um iris mom is crashing out on you now
+and i have no idea who tf hannah is but you two are like getting crashed out upon
+and DO NOT SAY THAT I TOLD YOU AYTHIN THATS HAPPENING 😭
+
+do NOT talk about this in discord dms cuz my mom might just check my discord idk (she knows my password i trust her idec if she checks but for this..)
+whenver we talk about this just say something like
+"yo i added a new segment of code into readme.cpp!!!" and i'll get the hint
+
+cya and gl!
+
+meanwhile, i'm going to meditate cuz i'm confused asf as to what happened
+and rechelle seems like a nice kid LMAO
