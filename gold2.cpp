551e62e89bb99d837ba325358b494d3d0d9a7176
cw0527 <iscurt.w@gmail.com>
2025-03-22 11:24:13 -0700

    gold 1, 2, 3

--- gold2.cpp
+++ gold2.cpp
@@ -1,125 +1,72 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-
-int N, Q;
-vector<int> fArr; 
-vector<int> seg;  
- 
-
+const int INF = 1e9;
+struct Node { int maxVal, maxIdx, minActive, maxActive; };
+int N, Q; 
+vector<int> freq;
+vector<Node> seg;
+Node mergeN(const Node &L, const Node &R) {
+    Node res;
+    if(L.maxVal >= R.maxVal) { res.maxVal = L.maxVal; res.maxIdx = L.maxIdx; }
+    else { res.maxVal = R.maxVal; res.maxIdx = R.maxIdx; }
+    res.minActive = min(L.minActive, R.minActive);
+    res.maxActive = max(L.maxActive, R.maxActive);
+    return res;
+}
 void build(int idx, int l, int r) {
     if(l == r) {
-        seg[idx] = fArr[l];
+        seg[idx].maxVal = freq[l];
+        seg[idx].maxIdx = l;
+        seg[idx].minActive = (freq[l] > 0 ? l : INF);
+        seg[idx].maxActive = (freq[l] > 0 ? l : -1);
         return;
     }
-    int mid = (l + r) / 2;
-    build(idx*2, l, mid);
-    build(idx*2+1, mid+1, r);
-    seg[idx] = max(seg[idx*2], seg[idx*2+1]);
+    int m = (l + r) / 2;
+    build(idx*2, l, m); build(idx*2+1, m+1, r);
+    seg[idx] = mergeN(seg[idx*2], seg[idx*2+1]);
 }
- 
-
-void updateSeg(int idx, int l, int r, int pos, int val) {
+void upd(int idx, int l, int r, int pos, int val) {
     if(l == r) {
-        seg[idx] = val;
+        freq[pos] = val;
+        seg[idx].maxVal = val;
+        seg[idx].maxIdx = pos;
+        seg[idx].minActive = (val > 0 ? pos : INF);
+        seg[idx].maxActive = (val > 0 ? pos : -1);
         return;
     }
-    int mid = (l + r) / 2;
-    if(pos <= mid)
-        updateSeg(idx*2, l, mid, pos, val);
-    else
-        updateSeg(idx*2+1, mid+1, r, pos, val);
-    seg[idx] = max(seg[idx*2], seg[idx*2+1]);
+    int m = (l + r) / 2;
+    if(pos <= m) upd(idx*2, l, m, pos, val);
+    else         upd(idx*2+1, m+1, r, pos, val);
+    seg[idx] = mergeN(seg[idx*2], seg[idx*2+1]);
 }
- 
-
-int findLeftmost(int idx, int l, int r, int target) {
-    if(l == r) return l;
-    int mid = (l + r) / 2;
-    if(seg[idx*2] == target)
-        return findLeftmost(idx*2, l, mid, target);
-    else
-        return findLeftmost(idx*2+1, mid+1, r, target);
-}
- 
-
-int findRightmost(int idx, int l, int r, int target) {
-    if(l == r) return l;
-    int mid = (l + r) / 2;
-    if(seg[idx*2+1] == target)
-        return findRightmost(idx*2+1, mid+1, r, target);
-    else
-        return findRightmost(idx*2, l, mid, target);
-}
- 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
     cin >> N >> Q;
-    
-    vector<int> cowVote(N+1);
-    fArr.resize(N+1, 0);
-    for (int i = 1; i <= N; i++){
-        cin >> cowVote[i];
-        fArr[cowVote[i]]++;
+    freq.resize(N+1, 0);
+    vector<int> cow(N+1);
+    for(int i = 1; i <= N; i++){
+        cin >> cow[i];
+        freq[cow[i]]++;
     }
-    
-    
-    set<int> nonzero;
-    for (int i = 1; i <= N; i++){
-        if(fArr[i] > 0) nonzero.insert(i);
-    }
-    
-    
-    seg.resize(4*(N+1), 0);
+    seg.resize(4*(N+1));
     build(1, 1, N);
-    
-    
-    auto getAnswer = [&]() -> int {
-        if(nonzero.size() < 2) return 0;
-        int L = *nonzero.begin();
-        int R = *nonzero.rbegin();
-        int freqL = fArr[L], freqR = fArr[R];
-        int M = seg[1]; 
-        
-        if(freqL + freqR >= M) return R - L;
-        
-        int leftM = findLeftmost(1, 1, N, M);
-        int rightM = findRightmost(1, 1, N, M);
-        return max(R - leftM, rightM - L);
-    };
-    
-    
-    
-    for (int qi = 0; qi < Q; qi++){
-        int i, x;
+    while(Q--){
+        int i, x; 
         cin >> i >> x;
-        int oldVote = cowVote[i];
-        if(oldVote == x){
-            cout << getAnswer() << "\n";
-            continue;
+        int old = cow[i];
+        cow[i] = x;
+        upd(1, 1, N, old, freq[old]-1);
+        upd(1, 1, N, x, freq[x]+1);
+        int Fmax = seg[1].maxVal, p = seg[1].maxIdx;
+        int L = seg[1].minActive, R = seg[1].maxActive;
+        int ans = 0;
+        if(L < R) {
+            int c1 = max(p - L, R - p);
+            int c2 = (freq[L] + freq[R] >= Fmax ? R - L : 0);
+            ans = max(c1, c2);
         }
-        cowVote[i] = x;
-        
-        
-        int oldVal = fArr[oldVote];
-        fArr[oldVote]--;
-        int newValOld = fArr[oldVote];
-        updateSeg(1, 1, N, oldVote, newValOld);
-        if(oldVal > 0 && newValOld == 0)
-            nonzero.erase(oldVote);
- 
-        
-        int oldVal2 = fArr[x];
-        fArr[x]++;
-        int newValX = fArr[x];
-        updateSeg(1, 1, N, x, newValX);
-        if(oldVal2 == 0 && newValX > 0)
-            nonzero.insert(x);
- 
-        cout << getAnswer() << "\n";
+        cout << ans << "\n";
     }
-    
     return 0;
 }
