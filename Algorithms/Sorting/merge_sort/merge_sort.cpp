7e301161a3b03389badfbca2a59fd3bbc3aaab91
Curtis Wei <iscurt.w@gmail.com>
2026-01-24 17:29:08 -0800

    Using std namespace declaration for clarity

--- Algorithms/Sorting/merge_sort/merge_sort.cpp
+++ Algorithms/Sorting/merge_sort/merge_sort.cpp
@@ -6,16 +6,18 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
 namespace {
 
 template <typename T>
-void merge(std::vector<T> &arr, std::size_t start, std::size_t end) {
-  std::size_t mid = (start + end) / 2;
-  std::vector<T> temp(end - start + 1);
+void merge(vector<T> &arr, size_t start, size_t end) {
+  size_t mid = (start + end) / 2;
+  vector<T> temp(end - start + 1);
 
-  std::size_t i = 0;
-  std::size_t left = start;
-  std::size_t right = mid + 1;
+  size_t i = 0;
+  size_t left = start;
+  size_t right = mid + 1;
 
   while (left <= mid && right <= end) {
     if (arr[left] < arr[right]) {
@@ -33,15 +35,15 @@ void merge(std::vector<T> &arr, std::size_t start, std::size_t end) {
     temp[i++] = arr[right++];
   }
 
-  for (std::size_t j = start; j <= end; ++j) {
+  for (size_t j = start; j <= end; ++j) {
     arr[j] = temp[j - start];
   }
 }
 
 template <typename T>
-void merge_sort_impl(std::vector<T> &arr, std::size_t start, std::size_t end) {
+void merge_sort_impl(vector<T> &arr, size_t start, size_t end) {
   if (start < end) {
-    std::size_t mid = (start + end) / 2;
+    size_t mid = (start + end) / 2;
     merge_sort_impl(arr, start, mid);
     merge_sort_impl(arr, mid + 1, end);
     merge(arr, start, end);
@@ -51,14 +53,14 @@ void merge_sort_impl(std::vector<T> &arr, std::size_t start, std::size_t end) {
 } // namespace
 
 template <typename T>
-void merge_sort(std::vector<T> &arr) {
+void merge_sort(vector<T> &arr) {
   if (!arr.empty()) {
     merge_sort_impl(arr, 0, arr.size() - 1);
   }
 }
 
 // Explicit template instantiations
-template void merge_sort<int>(std::vector<int> &arr);
-template void merge_sort<float>(std::vector<float> &arr);
-template void merge_sort<double>(std::vector<double> &arr);
-template void merge_sort<char>(std::vector<char> &arr);
+template void merge_sort<int>(vector<int> &arr);
+template void merge_sort<float>(vector<float> &arr);
+template void merge_sort<double>(vector<double> &arr);
+template void merge_sort<char>(vector<char> &arr);
--- Algorithms/Sorting/selection_sort/selection_sort.cpp
+++ Algorithms/Sorting/selection_sort/selection_sort.cpp
@@ -7,16 +7,18 @@
 #include <iterator>
 #include <vector>
 
+using namespace std;
+
 template <typename T>
-void selection_sort(std::vector<T> &arr) {
-  for (auto it = std::begin(arr); it != std::end(arr); ++it) {
-    auto const min_elem = std::min_element(it, std::end(arr));
-    std::iter_swap(min_elem, it);
+void selection_sort(vector<T> &arr) {
+  for (auto it = begin(arr); it != end(arr); ++it) {
+    auto const min_elem = min_element(it, end(arr));
+    iter_swap(min_elem, it);
   }
 }
 
 // Explicit template instantiations
-template void selection_sort<int>(std::vector<int> &arr);
-template void selection_sort<float>(std::vector<float> &arr);
-template void selection_sort<double>(std::vector<double> &arr);
-template void selection_sort<char>(std::vector<char> &arr);
+template void selection_sort<int>(vector<int> &arr);
+template void selection_sort<float>(vector<float> &arr);
+template void selection_sort<double>(vector<double> &arr);
+template void selection_sort<char>(vector<char> &arr);
